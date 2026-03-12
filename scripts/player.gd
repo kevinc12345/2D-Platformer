4b18a9c7f3845ffc0c25c4c568f69e7aca285cfc
kevinCai12345 <kecai2@s.sfusd.edu>
2026-03-12 11:39:26 -0700

    Checkpoint 2

---
+++ scripts/player.gd
@@ -0,0 +1,51 @@
+extends CharacterBody2D
+
+@export var move_speed : float = 100
+@export var accceleration : float =50
+@export var braking : float = 20
+@export var gravity : float = 500
+@export var jump_force : float = 200
+
+var move_input : float
+
+@onready var sprite : Sprite2D =$sprite
+@onready var anim : AnimationPlayer = $AnimationPlayer
+
+
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	move_input = Input.get_axis("move_left", "move_right")
+	
+	velocity.x = move_input * move_speed
+	
+	if move_input != 0:
+		velocity.x = lerp(velocity.x, move_input * move_speed,accceleration * delta)
+	
+	else:
+		velocity.x = lerp(velocity.x, 0.0, braking * delta)
+	
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = -jump_force
+
+	move_and_slide()
+	
+@warning_ignore("unused_parameter")
+func _process(delta):
+	sprite.flip_h = velocity.x>0
+	
+	_manage_animation()
+	
+func _manage_animation ():
+	if not is_on_floor():
+		anim.play("jump")
+	
+	elif move_input != 0:
+		anim.play("move")
+	
+	else:
+		anim.play("idle")
+	
+	
